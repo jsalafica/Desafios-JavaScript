3a24fdde594b29dbe37611490e0280b503049ec3
Javier Salafica <jsalafica@gmail.com>
2022-06-10 20:12:10 -0300

    cambios

--- js/index.js
+++ js/index.js
@@ -1,17 +1,37 @@
-let cantidadCamas = parseInt(prompt('Ingrese la cantidad de camas: '));
-document.write(`La cantidad de camas es: ${cantidadCamas} <br>`);
+let cantidadCamas = 0;
+let noEsNumero = true;
+do{
+    cantidadCamas = prompt('Ingrese la cantidad de camas disponibles: ');
+    if(!isNaN(cantidadCamas)){
+        noEsNumero = false;
+    }
+} while (noEsNumero);
+document.write(`La cantidad de camas disponibles es: ${cantidadCamas} <br>`);
+console.log(`La cantidad de camas disponibles es: ${cantidadCamas}`);
 let paciente = '';
 let cama = 0;
+let camaOcupada = 0;
 do {
-    paciente = prompt('Ingrese el nombre del paciente: (Escriba ESC para salir)');
-    if(paciente=="ESC"){
+    paciente = prompt('Ingrese el nombre del paciente: (Escriba Fin para terminar)').toUpperCase();
+    if(paciente=="FIN"){
         break;
     }
     cama = parseInt(prompt('Ingrese el numero de la cama: '));
     document.write(`El paciente ${paciente} se internó en la cama ${cama} <br>`);
+    console.log(`El paciente ${paciente} se internó en la cama ${cama}`);
+    camaOcupada++;
     cantidadCamas--;
     if(cantidadCamas==0){
         alert('No hay mas camas libres');
         break;
     }
-} while (paciente!='ESC');
+} while (paciente!='FIN');
+if(camaOcupada==1){
+    document.write(`Se ocupó ${camaOcupada} cama <br>`);
+    console.log(`Se ocupó ${camaOcupada} cama`);
+} else {
+    document.write(`Se ocuparon ${camaOcupada} camas <br>`);
+    console.log(`Se ocuparon ${camaOcupada} camas`);
+}
+document.write(`La cantidad de camas libres es: ${cantidadCamas}`);
+console.log(`La cantidad de camas libres es: ${cantidadCamas}`);
